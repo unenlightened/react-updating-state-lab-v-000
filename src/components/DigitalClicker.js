393341d20bcd58a6a720b140a6c306cfd2ed1d61
Chely Ho <adept_inept@yahoo.com>
2018-09-24 20:20:13 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
 export default class DigitalClicker extends Component {
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
 
   render() {
-
+    return (
+      <button onClick={this.handleClick}>Boop</button>
+    )
   }
 }
